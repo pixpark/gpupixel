cf7ee9cf11f575dfb65f9ffa4acae75a5628ced9
jaaron <jaaronkot@gmail.com>
2023-08-06 15:20:07 +0800

    rm no use bundle file

--- objc/demo/PixDemo/VideoFilterController.h
+++ objc/demo/PixDemo/VideoFilterController.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CameraFilterController : UIViewController
+@interface VideoFilterController : UIViewController
 
 @property(nonatomic, assign) CGFloat beautyValue;
 @property(nonatomic, assign) CGFloat whithValue;
