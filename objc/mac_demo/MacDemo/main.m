e318e7bf26935cf7e8097022a579f99999fbd824
Jaaron Kot <jaaronkot@gmail.com>
2024-01-21 00:36:09 +0800

    Add mac demo and smooth&white filter work ok

---
+++ objc/mac_demo/MacDemo/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SimpleVideoFilter
+//
+//  Created by gezhaoyou on 2021/8/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
