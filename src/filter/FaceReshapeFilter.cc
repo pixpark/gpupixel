c348aac906837b0cdcf7930f075b5ba9bf10492c
Jaaron Kot <jaaronkot@gmail.com>
2024-01-21 15:15:23 +0800

    refine android ios and mac demo

--- src/filter/FaceReshapeFilter.cc
+++ src/filter/FaceReshapeFilter.cc
@@ -128,11 +128,11 @@ bool FaceReshapeFilter::init() {
   }
     registerProperty("thin_face", 0, "The smoothing of filter with range between -1 and 1.", [this](float& val) {
         setFaceSlimLevel(val);
-    }
+    });
 
     registerProperty("big_eye", 0, "The smoothing of filter with range between -1 and 1.", [this](float& val) {
         setEyeZoomLevel(val);
-    }
+    });
 
   this->thinFaceDelta_ = 0.0;
   // [0, 0.15]
