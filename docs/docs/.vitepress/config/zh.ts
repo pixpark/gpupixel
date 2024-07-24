163be910e51f55562b22f4a5887cd435256e3dd2
Jarron Kot <gezhaoyou@126.com>
2024-07-24 12:57:01 +0800

    update docs

--- docs/docs/.vitepress/config/zh.ts
+++ docs/docs/.vitepress/config/zh.ts
@@ -16,7 +16,7 @@ export const zh = defineConfig({
     },
 
     editLink: {
-      pattern: 'https://github.com/pixpark/gpupixel/edit/main/docs/:path',
+      pattern: 'https://github.com/pixpark/gpupixel/edit/main/docs/docs/:path',
       text: '在 GitHub 上编辑此页面'
     },
 
