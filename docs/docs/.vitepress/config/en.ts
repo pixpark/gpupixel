001597eba3e2a61ef4874fc67174357636227b9a
Jaaron <jaaronkot@gmail.com>
2025-03-19 16:55:58 +0800

    update doc site edit link

--- docs/docs/.vitepress/config/en.ts
+++ docs/docs/.vitepress/config/en.ts
@@ -15,7 +15,7 @@ export const en = defineConfig({
     },
 
     editLink: {
-      pattern: 'https://github.com/pixpark/gpupixel/edit/doc/docs/docs/:path',
+      pattern: 'https://github.com/pixpark/gpupixel/edit/main/docs/docs/:path',
       text: 'Edit this page on GitHub'
     },
 
